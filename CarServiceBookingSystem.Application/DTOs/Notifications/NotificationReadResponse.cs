7eae77d8c2ad0555823cfb70640d0cc3e94bad1e
Mahmoud Mahmoud <mmmahmoudd@gmail.com>
2026-06-12 20:31:02 +0400

    add dtos and validators for notification

---
+++ CarServiceBookingSystem.Application/DTOs/Notifications/NotificationReadResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CarServiceBookingSystem.Application.DTOs.Notifications;
+
+public class NotificationReadResponse
+{
+    public int NotificationId { get; set; }
+
+    public bool IsRead { get; set; }
+
+    public DateTime? ReadAt { get; set; }
+}
---
+++ CarServiceBookingSystem.Application/DTOs/Notifications/UnreadNotificationCountResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace CarServiceBookingSystem.Application.DTOs.Notifications;
+
+public class UnreadNotificationCountResponse
+{
+    public int Count { get; set; }
+}
